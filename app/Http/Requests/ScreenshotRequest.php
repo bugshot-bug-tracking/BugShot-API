437786b69959e7936e1637401889d7ebcb0be7a2
Radu Memetea <r.memetea99@proxima-idea.com>
2021-09-21 13:38:13 +0300

    Screenshot input validation, relationship, storage

--- app/Http/Requests/ScreenshotRequest.php
+++ app/Http/Requests/ScreenshotRequest.php
@@ -24,13 +24,12 @@ class ScreenshotRequest extends FormRequest
 	public function rules()
 	{
 		return [
-			"bug_id" => ["required", "exists:App\Models\Bug,id"],
-			"designation" => ["required", "max:255"],
-			"url" => ["required"],
-			"position_x" => ["integer"],
-			"position_y" => ["integer"],
+			"bug_id" => ["required", "integer", "exists:App\Models\Bug,id"],
+			"position_x" => ["required", "integer"],
+			"position_y" => ["required", "integer"],
 			"web_position_x" => ["integer"],
 			"web_position_y" => ["integer"],
+			"file" => ["required", "file", "max:5200"],
 		];
 	}
 }
