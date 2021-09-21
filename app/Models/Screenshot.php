9b2a391927c1d144d1bf0732e694081c3b3202c8
Radu Memetea <r.memetea99@proxima-idea.com>
2021-09-21 13:38:13 +0300

    Screenshot input validation, relationship, storage

--- app/Models/Screenshot.php
+++ app/Models/Screenshot.php
@@ -10,4 +10,11 @@ class Screenshot extends Model
 	use HasFactory;
 
 	protected $fillable = ["bug_id", "designation", "url", "position_x", "position_y", "web_position_x", "web_position_y"];
+
+	protected $touches = ["bug"];
+
+	public function bug()
+	{
+		return $this->belongsTo(Bug::class);
+	}
 }
