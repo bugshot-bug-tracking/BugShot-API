17da17bbcce626bfaf8e0d4f9edea533b519fcae
Radu Memetea <r.memetea99@proxima-idea.com>
2021-09-23 16:35:21 +0300

    Model documentations

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -5,9 +5,48 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @OA\Schema()
+ */
 class Image extends Model
 {
 	use HasFactory;
+
+	/**
+	 * @OA\Property(
+	 * 	property="id",
+	 * 	type="integer",
+	 *  format="int64",
+	 * )
+	 *
+	 * @OA\Property(
+	 * 	property="designation",
+	 * 	type="string",
+	 *  maxLength=255,
+	 * 	description="The file name."
+	 * )
+	 *
+	 * @OA\Property(
+	 * 	property="url",
+	 * 	type="string",
+	 *  format="date-time",
+	 * 	description="The lacal path where the file is stored."
+	 * )
+	 *
+	 * @OA\Property(
+	 * 	property="created_at",
+	 * 	type="string",
+	 *  format="date-time",
+	 * 	description="The creation date."
+	 * )
+	 *
+	 * @OA\Property(
+	 * 	property="updated_at",
+	 * 	type="string",
+	 *  format="date-time",
+	 * 	description="The last date when the resource was changed."
+	 * )
+	 */
 
 	protected $fillable = ["designation", "url"];
 }
--- app/Models/Project.php
+++ app/Models/Project.php
@@ -5,9 +5,70 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @OA\Schema()
+ */
 class Project extends Model
 {
 	use HasFactory;
+
+	/**
+	 * @OA\Property(
+	 * 	property="id",
+	 * 	type="integer",
+	 *  format="int64",
+	 * )
+	 *
+	 * @OA\Property(
+	 * 	property="company_id",
+	 * 	type="integer",
+	 *  format="int64",
+	 * 	description="The id of the company to which the object belongs."
+	 * )
+	 *
+	 * @OA\Property(
+	 * 	property="image_id",
+	 * 	type="integer",
+	 *  format="int64",
+	 * 	nullable=true,
+	 * 	description="The id of the image that belongs to the project."
+	 * )
+	 *
+	 * @OA\Property(
+	 * 	property="designation",
+	 * 	type="string",
+	 *  maxLength=255,
+	 * 	description="The status name."
+	 * )
+	 *
+	 * @OA\Property(
+	 * 	property="url",
+	 * 	type="string",
+	 * 	description="The url of the project. (Validated)"
+	 * )
+	 *
+	 * @OA\Property(
+	 * 	property="created_at",
+	 * 	type="string",
+	 *  format="date-time",
+	 * 	description="The creation date."
+	 * )
+	 *
+	 * @OA\Property(
+	 * 	property="updated_at",
+	 * 	type="string",
+	 *  format="date-time",
+	 * 	description="The last date when the resource was changed."
+	 * )
+	 *
+	 * @OA\Property(
+	 * 	property="deleted_at",
+	 * 	type="string",
+	 *  format="date-time",
+	 * 	description="The deletion date."
+	 * )
+	 *
+	 */
 
 	protected $fillable = ["designation", "url", "company_id", "image_id"];
 
