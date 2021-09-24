fd672a50ea02ae1fb4933afbd149a1406be313e6
Radu Memetea <r.memetea99@proxima-idea.com>
2021-09-24 15:12:32 +0300

    Documented all controllers/routes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,9 +12,48 @@ use App\Models\ProjectUserRole;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
+
+/**
+ * @OA\Tag(
+ *     name="User",
+ * )
+ */
 class UserController extends Controller
 {
 
+	/**
+	 * @OA\Get(
+	 *	path="/user/companies",
+	 *	tags={"User"},
+	 *	summary="Show all companies that a use works at.",
+	 *	operationId="showUserCompanies",
+	 *	security={ {"sanctum": {} }},
+	 *
+	 *	@OA\Response(
+	 *		response=200,
+	 *		description="Success",
+	 *		@OA\JsonContent(
+	 *			ref="#/components/schemas/CompanyUserRole"
+	 *		)
+	 *	),
+	 *	@OA\Response(
+	 *		response=400,
+	 *		description="Bad Request"
+	 *	),
+	 *	@OA\Response(
+	 *		response=401,
+	 *		description="Unauthenticated"
+	 *	),
+	 *	@OA\Response(
+	 *		response=403,
+	 *		description="Forbidden"
+	 *	),
+	 *	@OA\Response(
+	 *		response=404,
+	 *		description="Not Found"
+	 *	),
+	 * )
+	 **/
 	/**
 	 * Display all companies whom the user is affiliated with
 	 *
@@ -33,6 +72,49 @@ class UserController extends Controller
 		return response()->json("", 200);
 	}
 
+	/**
+	 * @OA\Get(
+	 *	path="/user/company/{id}/projects",
+	 *	tags={"User"},
+	 *	summary="Show all projects from a company that a use is a part of.",
+	 *	operationId="showUserCompanyProjects",
+	 *	security={ {"sanctum": {} }},
+	 *
+	 *	@OA\Parameter(
+	 *		name="id",
+	 *		required=true,
+	 *		in="path",
+	 *		@OA\Schema(
+	 *			ref="#/components/schemas/Company/properties/id"
+	 *		)
+	 *	),
+	 *
+	 *	@OA\Response(
+	 *		response=200,
+	 *		description="Success",
+	 *		@OA\JsonContent(
+	 *			type="array",
+	 *			@OA\Items(ref="#/components/schemas/ProjectUserRole")
+	 *		)
+	 *	),
+	 *	@OA\Response(
+	 *		response=400,
+	 *		description="Bad Request"
+	 *	),
+	 *	@OA\Response(
+	 *		response=401,
+	 *		description="Unauthenticated"
+	 *	),
+	 *	@OA\Response(
+	 *		response=403,
+	 *		description="Forbidden"
+	 *	),
+	 *	@OA\Response(
+	 *		response=404,
+	 *		description="Not Found"
+	 *	),
+	 * )
+	 **/
 	/**
 	 * Display all projects from a company where the user is a part of
 	 *
@@ -59,6 +141,54 @@ class UserController extends Controller
 		return response()->json($projects, 200);
 	}
 
+	/**
+	 * @OA\Post(
+	 *	path="/check-project",
+	 *	tags={"User"},
+	 *	summary="Return a project with the specified url where the user is a part of",
+	 *	operationId="checkProject",
+	 *	security={ {"sanctum": {} }},
+	 *
+	 *  @OA\RequestBody(
+	 *      required=true,
+	 *      @OA\MediaType(
+	 *          mediaType="application/json",
+	 *          @OA\Schema(
+	 *  			@OA\Property(
+	 *                  property="url",
+	 *                  type="string",
+	 *              ),
+	 *              required={"url"}
+	 *          )
+	 *      )
+	 *  ),
+	 *
+	 *	@OA\Response(
+	 *		response=200,
+	 *		description="Success",
+	 *		@OA\JsonContent(
+	 *			type="array",
+	 *			@OA\Items(ref="#/components/schemas/ProjectUserRole")
+	 *		)
+	 *	),
+	 *	@OA\Response(
+	 *		response=400,
+	 *		description="Bad Request"
+	 *	),
+	 *	@OA\Response(
+	 *		response=401,
+	 *		description="Unauthenticated"
+	 *	),
+	 *	@OA\Response(
+	 *		response=403,
+	 *		description="Forbidden"
+	 *	),
+	 *	@OA\Response(
+	 *		response=404,
+	 *		description="Not Found"
+	 *	),
+	 * )
+	 **/
 	public function checkProject(Request $request)
 	{
 		$request->validate([
