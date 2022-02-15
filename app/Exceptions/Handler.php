71df52dac70c7a5dc95942a8f2732dd33a01bd4c
Tim Slabon <t.slabon@it-michel.de>
2022-02-15 09:03:36 +0100

    Adds sentry to the project

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -38,4 +38,14 @@ class Handler extends ExceptionHandler
             //
         });
     }
+
+    // Reporting for Sentry tool
+    public function report(Throwable $exception)
+    {
+        if (app()->bound('sentry') && $this->shouldReport($exception)) {
+            app('sentry')->captureException($exception);
+        }
+
+        parent::report($exception);
+    }
 }
