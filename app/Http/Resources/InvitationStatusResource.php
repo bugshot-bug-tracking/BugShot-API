d70880590981ca331debf59c93880be96a233e74
Tim Slabon <t.slabon@it-michel.de>
2022-04-26 11:29:43 +0200

    Fixes bug where the wrong resource was chosen for the invitation status

--- app/Http/Resources/InvitationStatusResource.php
+++ app/Http/Resources/InvitationStatusResource.php
@@ -14,6 +14,12 @@ class InvitationStatusResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+		return [
+			'id' => $this->id,
+			'type' => 'InvitationStatus',
+			'attributes' => [
+				'designation' => $this->designation
+			]
+		];
     }
 }
