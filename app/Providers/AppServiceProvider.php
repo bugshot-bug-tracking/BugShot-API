00607ee1fd09d39b690c8885bed2bb9b2c1733ac
Tim Slabon <t.slabon@it-michel.de>
2022-07-01 11:59:45 +0200

    Fixes tinyfy bug

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Tinify\Tinify;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,6 +26,6 @@ class AppServiceProvider extends ServiceProvider
 	public function boot()
 	{
 		Schema::defaultStringLength(255);
-		\Tinify\setKey(config('app.tinypng_api_key'));
+		Tinify::setKey(config('app.tinypng_api_key'));
 	}
 }
