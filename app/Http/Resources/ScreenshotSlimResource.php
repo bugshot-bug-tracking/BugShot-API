5e7af64c8fc48e3317850bc40e38a1fe7a96b77a
Felix Wildemann <f.wildemann@it-michel.de>
2023-03-07 13:31:24 +0100

    fixes

---
+++ app/Http/Resources/ScreenshotSlimResource.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ScreenshotSlimResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+	{
+		$screenshot = array(
+			"id" => $this->id,
+			"type" => "Screenshot",
+			"attributes" => [
+				"bug_id" => $this->bug_id,
+				"client_id" => $this->client_id,
+				"url" => $this->url,
+				"position_x" => $this->position_x,
+				"position_y" => $this->position_y,
+				"web_position_x" => $this->web_position_x,
+				"web_position_y" => $this->web_position_y,
+			]
+		);
+
+		$header = $request->header();
+
+		// Check if the response should contain the respective screenshots
+		if(array_key_exists('include-markers', $header) && $header['include-markers'][0] == "true") {
+			$markers = $this->markers;
+			$screenshot['attributes']['markers'] = MarkerResource::collection($markers);
+		}
+
+		return $screenshot;
+	}
+}
