dd947decdb0f73eb25ad7629c103d6730298bcc6
Tim Slabon <t.slabon@it-michel.de>
2023-03-08 11:03:11 +0100

    Adds Scriptcontroller and script to compress all old images/screenshots

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -67,5 +67,6 @@ class Kernel extends HttpKernel
 		'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
 		'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
 		'check.version' => \App\Http\Middleware\CheckVersion::class,
+		'scripts.active' => \App\Http\Middleware\ScriptActiveMiddleware::class,
 	];
 }
