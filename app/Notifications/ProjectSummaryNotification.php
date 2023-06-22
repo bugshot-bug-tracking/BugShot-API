e87b78286ffccbdd41264d945e17144b8bc43cfe
Tim Slabon <t.slabon@it-michel.de>
2023-06-22 22:31:35 +0200

    Start implementing of daily project summary notifications

---
+++ app/Notifications/ProjectSummaryNotification.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Notifications;
+
+// Miscellaneous, Helpers, ...
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use App\Mail\CommentCreated as CommentCreatedMailable;
+
+class ProjectSummaryNotification extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+	public $comment;
+	// TODO: Refactor this class from commen to project
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($comment)
+    {
+        $this->comment = $comment;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail', 'database'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new CommentCreatedMailable($notifiable, $this->locale, $this->comment))
+        ->subject('BugShot - ' . __('email.comment-created', [], $this->locale))
+        ->to($notifiable->email);
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+			"type" => "CommentCreated",
+            "data" => [
+				"creator_name" => $this->comment->user->first_name . " " . $this->comment->user->last_name,
+				"organization_id" => $this->comment->bug->project->company->organization->id,
+				"company_id" => $this->comment->bug->project->company->id,
+				"project_id" => $this->comment->bug->project->id,
+				"bug_id" => $this->comment->bug->id,
+				"comment_id" => $this->comment->id,
+				"created_at" => $this->comment->created_at
+			]
+        ];
+    }
+}
