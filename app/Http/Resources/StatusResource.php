ac7a8a6cac87f0386d21a097472582a3a30172e8
Tim Slabon <t.slabon@it-michel.de>
2023-08-31 07:51:53 +0200

    Add filters for fetching bugs to all relevant resources

--- app/Http/Resources/StatusResource.php
+++ app/Http/Resources/StatusResource.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Resources;
 
+use App\Models\Priority;
+use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -32,17 +34,37 @@ class StatusResource extends JsonResource
 
 		// Check if the response should contain the respective bugs
 		if(array_key_exists('include-bugs', $header) && $header['include-bugs'][0] == "true") {
-			if(array_key_exists('only-assigned-bugs', $header) && $header['only-assigned-bugs'][0] == "true") {
-				$bugs = Auth::user()->bugs()
-						->where("status_id", $this->id)
-						->where("bugs.archived_at", NULL)
-						->get();
-
-				$status['attributes']['bugs'] = BugResource::collection($bugs);
+			if(array_key_exists('filter-bugs-by-assigned', $header) && $header['filter-bugs-by-assigned'][0] == "true") {
+				$bugs = Auth::user()->bugs();
 			} else {
-				$bugs = $this->bugs()->where("bugs.archived_at", NULL)->get();
-				$status['attributes']['bugs'] = BugResource::collection($bugs);
+				$bugs = $this->bugs();
 			}
+
+			// Add filters to the query
+			$bugs = $bugs
+				->when(array_key_exists('filter-bugs-by-deadline', $header) && !empty($header['filter-bugs-by-deadline'][0]), function ($query) use ($header) {
+					$deadline = $header['filter-bugs-by-deadline'][0];
+					$array = explode('|', $deadline);
+					$operator = $array[0];
+					$date = date("Y-m-d H:i:s", $array[1]);
+
+					return $query->where("deadline", $operator, $date);
+				})
+				->when(array_key_exists('filter-bugs-by-creator-id', $header) && !empty($header['filter-bugs-by-creator-id'][0]), function ($query) use ($header) {
+					$creatorId = $header['filter-bugs-by-creator-id'][0];
+
+					return $query->where("user_id", $creatorId);
+				})
+				->when(array_key_exists('filter-bugs-by-priority', $header) && !empty($header['filter-bugs-by-priority'][0]), function ($query) use ($header) {
+					$designation = $header['filter-bugs-by-priority'][0];
+					$priority = Priority::where('designation', $designation)->firstOrFail();
+
+					return $query->where("priority_id", $priority->id);
+				})
+				->where("bugs.archived_at", NULL)
+				->get();
+
+			$status['attributes']['bugs'] = BugResource::collection($bugs);
 		}
 
 		return $status;
