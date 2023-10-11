4de35601fefb6a52397a5f5befb6bec40ac1793e
Tim Slabon <t.slabon@it-michel.de>
2023-10-11 07:59:52 +0200

    feat (API): Extend action seeder by more actions

--- database/seeders/ActionSeeder.php
+++ database/seeders/ActionSeeder.php
@@ -16,19 +16,31 @@ class ActionSeeder extends Seeder
     public function run()
     {
 		$actions = [
-			"created",
-			"updated",
-			"deleted",
-			"moved_to_new_organization",
-			"moved_to_new_group",
-			"moved_to_new_project",
+			// Organizations (TODO: For now lets only do project actions)
+			// "organization_name_changed",
+			// "organization_deleted",
+			// "organization_group_moved_to_new_organization",
+
+			// Projects
+			"project_name_changed",
+			"project_moved_to_new_group",
+			"project_bug_status_changed",
+			"project_bug_deleted",
+			"project_bug_created",
+			"project_bug_data_updated",
+			"proejct_user_assigned_to_bug",
+			"proejct_user_assigment_to_bug_revoked",
+			"project_user_invited",
+			"project_bugs_moved_to_new_project",
+			"project_access_token_generated",
+			"project_access_token_deleted"
 			// TODO: Keep finding new actions and check if an history entry is made for a organization when e.g. a bug is moved to a new status
 		];
 
-		Action::insert([
-			[
-				"designation" => "created",
-			],
-		]);
+		foreach($actions as $action) {
+			Action::create([
+				"designation" => $action
+			]);
+		}
     }
 }
